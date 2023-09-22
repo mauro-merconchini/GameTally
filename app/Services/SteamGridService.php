a37736ff4d8108db36bc44f95c53b25d5f8b9a25
Javier Feliz <javier0eduardo@hotmail.com>
2023-09-22 15:12:33 -0400

    Start working on getting icon and creating game

--- app/Services/SteamGridService.php
+++ app/Services/SteamGridService.php
@@ -5,11 +5,23 @@ use Illuminate\Support\Facades\Http;
 
 class SteamGridService
 {
+    protected static function request()
+    {
+        $baseUrl = 'https://www.steamgriddb.com/api/v2/';
+        return Http::withToken('24dcf4ee16a28dcf66bf9a32a02fd802')->baseUrl($baseUrl);
+    }
+
     public static function getAutoCompleteResults(string $gameName): array
     {
-        $path = "https://www.steamgriddb.com/api/v2/search/autocomplete/$gameName";
-        $result = Http::withToken('24dcf4ee16a28dcf66bf9a32a02fd802')->get($path);
+        $result = self::request()->get("search/autocomplete/$gameName");
 
         return $result->json('data');
+    }
+
+    public static function getGameIcon(int $gameId)
+    {
+        $result = self::request()->get("icons/game/{$gameId}");
+
+        dd($result->json());
     }
 }
--- resources/views/games-search-result.blade.php
+++ resources/views/games-search-result.blade.php
@@ -1,5 +1,20 @@
 <x-app-layout>
-    @foreach ($gameArray as $game)
-        <p for="">{{$game['name']}}</p>
-    @endforeach
+    <div class="max-w-7xl mx-auto bg-white rounded-md px-2 py-4 mt-24">
+        @foreach ($gameArray as $game)
+        <div class="border-b border-gray-300 mb-4 py-4 px-2">
+            <form action="{{route('games.store')}}" class="block w-full">
+                <div class="flex justify-between">
+                    <div class="flex items-center">
+                        <p class="font-bold text-xl">{{$game['name']}}</p>
+                        <input type="hidden" name="name" value="{{$game['name']}}">
+                        <input type="hidden" name="game_id" value="{{$game['id']}}">
+                    </div>
+                    <div>
+                        <button type="submit" class="bg-indigo-600 px-2 py-4 rounded-lg text-md text-white">This one</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        @endforeach
+    </div>
 </x-app-layout>
