178481acf053b129ef2d0b61874e3a37cbdd9937
Javier Feliz <javier0eduardo@hotmail.com>
2023-09-25 17:03:27 -0400

    UI improvements

--- resources/views/livewire/game-list.blade.php
+++ resources/views/livewire/game-list.blade.php
@@ -1,12 +1,22 @@
 <div>
-    <div>
-        @foreach ($statuses as $s)
-            <div class="bg-gray-300 text-gray-900 px-2 py-4 mb-2">{{$s->name}}</div>
-
-            @foreach ($s->games as $game)
-                <livewire:game-list-item :game="$game" :key="$game->id" />
-            @endforeach
-            
+    <div x-data="{show: true}">
+        <div class="bg-gray-300 text-gray-900 px-2 py-4 mb-2">In Progress</div>
+        
+        @foreach ($statuses->where('name', 'In Progress')->first()->games as $game)
+            <livewire:game-list-item :game="$game" :key="$game->id" />
         @endforeach
     </div>
+    
+    @foreach ($statuses->where('name', '!=', 'In Progress')->all() as $s)
+        <div x-data="{show: false}">
+            <div x-on:click="show = !show" class="bg-gray-300 text-gray-900 px-2 py-4 mb-2">{{$s->name}}</div>
+        
+            <div x-show="show">
+        
+                @foreach ($s->games as $game)
+                <livewire:game-list-item :game="$game" :key="$game->id" />
+                @endforeach
+            </div>
+        </div>
+    @endforeach
 </div>
